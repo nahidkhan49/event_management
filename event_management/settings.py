a3a5bd4d4e560e49db839c8577abf28c0a278afd
nahidkhan49 <nahid822399@gmail.com>
2025-12-23 12:21:02 +0600

    ready for deplyment

--- event_management/settings.py
+++ event_management/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-799lq1j09^i60q35c0gu6qkk=86kezx00^cw23wmx2r6_@(t7&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS=['https://*.onrender.com','http://127.0.0.1:8000']
 
 
 # Application definition
