af7672d6dc5eafa99071b19f71fa13286d0447bc
nahidkhan49 <nahid822399@gmail.com>
2025-12-23 14:40:43 +0600

    root set

--- event_management/urls.py
+++ event_management/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path,include
 from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('events/',include("events.urls"))
+    path('events/',include("events.urls")),
+    path('', lambda request: redirect('events:event_list')),
 ]+debug_toolbar_urls()
