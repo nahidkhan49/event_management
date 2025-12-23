d5201ffbbafc826b36979024b7fa3aaa0445c5cc
nahidkhan49 <nahid822399@gmail.com>
2025-12-23 16:09:47 +0600

    time 5.00

--- event_management/urls.py
+++ event_management/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.shortcuts import redirect
 from django.urls import path,include
 from debug_toolbar.toolbar import debug_toolbar_urls
+from events import views as event_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', event_views.home, name='home'),
     path('events/',include("events.urls")),
 ]+debug_toolbar_urls()
